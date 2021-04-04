d496c4a2e06db227d283b4e7b7ec7375a9d73429
Денис Петровский <petrovsky.dv@gmail.com>
2021-04-04 21:31:01 +0300

    удалил поля из модели Flat

--- property/models.py
+++ property/models.py
@@ -5,14 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца',
-        null=True,
-        blank=True,
-        help_text='+79999999999'
-    )
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
