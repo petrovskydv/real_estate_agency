169da164b073f786525591f8b5de7e2caf532d2f
Денис Петровский <petrovsky.dv@gmail.com>
2021-04-04 21:44:14 +0300

    добавил отображение связи квартир и собственников

--- property/admin.py
+++ property/admin.py
@@ -5,6 +5,11 @@ from .models import Flat
 from .models import Owner
 
 
+class OwnersInline(admin.TabularInline):
+    model = Owner.flat.through
+    raw_id_fields = ('owner', 'flat')
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address',)
     readonly_fields = ['created_at']
@@ -12,6 +17,9 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
+    inlines = [
+        OwnersInline,
+    ]
 
 
 class ClaimAdmin(admin.ModelAdmin):
@@ -23,6 +31,9 @@ class OwnerAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'owner_pure_phone',)
     list_display = ('owner', 'owner_pure_phone',)
     raw_id_fields = ('flat',)
+    inlines = [
+        OwnersInline,
+    ]
 
 
 admin.site.register(Flat, FlatAdmin)
